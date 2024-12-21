d77abcfe2e536783f1f4e69b100dc3b0474fe09f
nox-bi11 <emmanuel.kichuki@gmail.com>
2024-12-21 10:41:30 +0300

    first-commit

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <div className="footer-container">
+      <p></p>
+    </div>
+  );
+};
+
+export default Footer;
